21258fda798226cc3293a10ae213b0cc748c178d
shuxiang <shuxiang29@outlook.com>
2025-12-19 13:20:42 +0800

    add lora train script

--- diamoe_tts/src/f5_tts/infer/batch_infer.sh
+++ diamoe_tts/src/f5_tts/infer/batch_infer.sh
@@ -5,18 +5,14 @@ CUDA_VISIBLE_DEVICES=0 python ./batch_infer.py \
 --expert_type MLP \
 --num_exps 9 \
 --moe_topK 1 \
---gen_file path/to/testset.txt \
---ckpt_file path/to/10ep_mlpEXP_9.pt \
---vocab_file diamoettsv1/diamoe_tts/data/vocab.txt  \
---ref_audio path/to/reference.wav \
---ref_text path/to/reference.txt \
---output_dir path/to/output_dir
+--gen_file /home/sx/diamoe/resources/test/test1.txt \
+--ckpt_file /home/sx/diamoe/resources/10ep_mlpEXP_9.pt \
+--vocab_file /home/sx/diamoe/diamoe_tts/data/vocab.txt  \
+--ref_audio /home/sx/diamoe/prompts/sinmin_main.wav \
+--ref_text /home/sx/diamoe/prompts/sinmin_main.txt \
+--output_dir /home/sx/diamoe/resources/test
 
 # if use peft model, add --lora-path path/to/lora.pt
-
-
-
-
 
 
 
